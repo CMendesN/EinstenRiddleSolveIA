86fe1717ebfa30fbf72e0ae1fe6290ccca5cb060
CMendesN <carlosmendes-neto@hotmail.com>
2022-11-19 02:35:02 -0300

    tests
    mudando variaveis para achar a melhor chance

--- algoritmogenetico.py
+++ algoritmogenetico.py
@@ -66,7 +66,7 @@ def getHouseByPet (individuo, k, pet):
             if(individuo[k].matriz[4* 5 + i] == pet):
                 return i
 def Evaluation(population, genAtual):
-    for i in range(0,population):
+    for i in range(len(individuo)):
         individuo[i].ponto = 0
         #  1 : O Norueguês vive na primeira casa.
         if(individuo[i].matriz[Col_Nacionality() + 0]== 3):
@@ -148,66 +148,42 @@ def Evaluation(population, genAtual):
         
     return sort()
         
-#cross_list = []       
+cross_list = []       
 def SelectParent(population, currGen):
-        '''
+        
         global cross_list
         for i in range(len(individuo)):
-            fitness =int ((individuo[i].ponto/15)*10)
+            fitness =int(((individuo[i].ponto/15)*10))
             for j in range(fitness):
                 cross_list.append(i)
-        '''
-        pass
-def CrossOver(population, currGen):
-        cross_list = []
+        random.shuffle(cross_list)
+        del(cross_list[population-int(population*0.5):len(cross_list)]) 
         
-        for i in range(len(individuo)):
-            cross = random.random()
-            if(cross < 0.81):
-                cross_list.append(i)
-        
+def CrossOver(population, currGen):             
         if (len(cross_list)%2 != 0):
                del cross_list[len(cross_list)-1]
         
-        random.shuffle(cross_list)        
-        for k in range(int(len(cross_list)/2)):            
-            if(k < len(cross_list)-1):
+                
+        for k in range(int(len(cross_list)/2)):
+            cross = random.random()
+            if( cross < 0.81):
                 individuo.append(House())
                 individuo.append(House())
                 individuo[len(individuo) - 2].matriz = individuo[cross_list[k]].matriz[0:10] + individuo[cross_list[len(cross_list)-1-k]].matriz[10:25]
                 individuo[len(individuo) - 1].matriz = individuo[cross_list[len(cross_list)-1-k]].matriz[0:10] + individuo[cross_list[k]].matriz[10:25]
-                #k = k+1
-                '''
-                for j in range(len(individuo[cross_list[k]].matriz)):
-                    if j < 10:
-                        individuo[len(individuo) - 2].matriz.insert(j, individuo[cross_list[k]].matriz[j])
-                    if j >= 10:
-                        individuo[len(individuo) - 1].matriz.insert(j, individuo[cross_list[k]].matriz[j])                    
-                k=k+1
-                for m in range(len(individuo[cross_list[k]].matriz)):
-                    if m < 10:
-                        individuo[len(individuo) - 1].matriz.insert(m, individuo[cross_list[k]].matriz[m])
-                    if m >= 10:
-                        individuo[len(individuo) - 2].matriz.insert(m, individuo[cross_list[k]].matriz[m]) 
-                '''
-               # print(individuo[len(individuo) - 2].matriz,"\n")
-               # print(individuo[len(individuo) - 1].matriz,"\n")
+                
         cross_list.clear()
-        '''
-        fazer a trocar pelas linhas. 
-        exemplo pegar a cor e a nacionalide de um e botar com a bebido o cigarro e o animal de outro.
-        assim evitando bugar a matriz com valores repetidos e excluindo outros.
-        '''
+        
         
 def mutacao1(i):
-    g = random.randint(1,5)
+    g = random.randint(1,16)
     for q in range(g):    
         selacaoDaCasa = Rng()
         selacaoDoTipo = Rng()
         posicaoDaTroca = selacaoDoTipo * 5 + selacaoDaCasa
         trocaDeDado = Rng()
         temp = individuo[i].matriz[posicaoDaTroca]
-        for k in range(4):        
+        for k in range(5):        
                 if individuo[i].matriz[selacaoDoTipo * 5 + k] == trocaDeDado :
                     individuo[i].matriz[posicaoDaTroca] = individuo[i].matriz[selacaoDoTipo * 5 + k]
                     individuo[i].matriz[selacaoDoTipo * 5 + k] = temp
@@ -246,29 +222,25 @@ def Mutation(population, currGen):
         
         for i in range(len(individuo)):
             mutar = random.random()
-            if(mutar < 0.35):
-                
-                if mutar<0.005:
-                    
-                    mutacao1(i)
-                else:
-                    
-                    mutacao1(i)
-            
+            if(mutar < 0.71):
+                #print(len(individuo))
+                mutacao1(i)
+                            
 flag = 0
 def Suvivors(population, currGen):
-    del(individuo[population-int(population*0.2):len(individuo)])
+    del(individuo[population-int(population*0.80):len(individuo)])
     global flag 
     flag = flag + 1
-    if(flag%15==0):
-        
-        for i in range(len(individuo), population):
+    #if(flag%15==0):
+    '''  
+    for i in range(len(individuo), population):
             individuo.insert(i, House())
             for linha in range(5):
                 valores = random.sample(const_list, len(const_list))
                 for coluna in range(5):        
                     individuo[i].matriz.append(valores[coluna])
-    '''
+    #Evaluation(population, currGen)
+    
     resposta=[0,1,4,3,2,3,1,2,0,4,0,3,4,1,2,2,0,3,4,1,2,1,3,4,0]
     individuo.insert(len(individuo), House())
     for k in range(25):
@@ -281,15 +253,15 @@ def Rng():
         return random.randint(0,4)
 def sort():
     individuoSort = sorted(individuo, key = cmp_to_key(compare))
-    for i in range(len(individuoSort)):
-        individuo[i] = individuoSort[i]
+    individuo.clear()
+    individuo.extend(individuoSort)
     individuo.reverse()
     
 def main():
     stop = st.StopWatch()
     currGen = 0
-    lastGen = 10000
-    population = 200
+    lastGen = 1000
+    population = 1000
     stop.start()
     Init_population(population, currGen)    
     Evaluation(population, currGen)
@@ -301,13 +273,14 @@ def main():
         Evaluation(population, currGen)        
         if(individuo[0].ponto == 15):
             break
-        print("Melhor da geracao: ",individuo[0].numero, "Pontos: ", individuo[0].ponto)
+        if(currGen%50 == 0):
+            print("Melhor da geracao: ",individuo[0].numero, "Pontos: ", individuo[0].ponto)
         Suvivors(population, currGen)
         
     
-    for i in range(len(individuo)*0.2):
+    for i in range(int(len(individuo)*0.2)):
         print(individuo[i].matriz,"\n", individuo[i].ponto)
-    print(len(individuo))
+    #print(len(individuo))
     
     print("Melhor individuo: ",individuo[0].numero,"\n",individuo[0].matriz,"\n", "Pontos: ", individuo[0].ponto)
     stop.stop()
